71d884106a0603bd88fae20bbec622696996d548
Denys Bokov <denisbokov1703@gmail.com>
2026-02-20 13:15:47 +0100

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,15 @@ import './App.css'
 import Transition from './Transition';
 import Optimistic from './Optimistic';
 import Form from './Form';
+import { Suspense } from 'react';
+import UseSuspense from './UseSuspense';
+import { mockServerError, mockServerSuccess } from './utils';
+import { ErrorBoundary } from 'react-error-boundary';
 
 function App() {
+  const helloWorldPromise = mockServerSuccess('hello world');
+  // const helloWorldPromise = mockServerError();
+
   return (
     <main>
       <h1>React v19</h1>
@@ -11,6 +18,11 @@ function App() {
       <Transition />
       <Optimistic />
       <Form />
+      <ErrorBoundary fallback="error....">
+        <Suspense fallback="loading....">
+          <UseSuspense promise={helloWorldPromise} />
+        </Suspense>
+      </ErrorBoundary>
     </main>
   )
 }
