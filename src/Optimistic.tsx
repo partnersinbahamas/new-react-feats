4911c1da712f3ebd3175b1ec6cb6940a189737ff
Denys Bokov <denisbokov1703@gmail.com>
2026-02-20 13:15:22 +0100

    update optimistic

--- src/Optimistic.tsx
+++ src/Optimistic.tsx
@@ -1,38 +1,37 @@
-import { useOptimistic, useState, useTransition } from "react";
+import { useActionState, useOptimistic, useState, useTransition } from "react";
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import { mockServerSuccess, mockServerError } from "./utils";
 
 const Optimistic = () => {
-    const [isPending, startTransition] = useTransition();
     const [like, setLike] = useState(false);
-    const [optimisticLike, setOptimisticList] = useOptimistic(like, (draft, value: boolean) => value);
+    const [optimisticLike, setOptimisticLike] = useOptimistic(like, (draft, value: boolean) => value);
     
+    const [_, likeAction] = useActionState(async() => {
+        try {
+            setOptimisticLike(true);
+            const respose = await mockServerSuccess(true);
+    
+            setLike(respose.data)
+        } catch(err) {
+            // ...
+        }
+    }, null)
 
-    const handleLike = async () => {
-        startTransition(async () => {
-            try {
-                setOptimisticList(true);
-                // const respose = await mockServerError();
-                const respose = await mockServerSuccess(true);
-                setLike(respose.data)
-            } catch(error) {
-                console.error('Error', error)
-            }
-        });
-    };
 
     return (
         <div>
         <h2>useOptimistic</h2>
         
-        <button type="submit" onClick={handleLike}>
-          {optimisticLike ? (
-            <FavoriteIcon />
-          ) : (
-            <FavoriteBorderIcon />
-          )}
-        </button>
+        <form action={likeAction}>
+            <button type="submit">
+                {optimisticLike ? (
+                    <FavoriteIcon />
+                ) : (
+                    <FavoriteBorderIcon />
+                )}
+            </button>
+        </form>
       </div>
     )
 }
